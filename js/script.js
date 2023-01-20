2ac122521cf644001cc2af9645f2f4308f7c4e27
Егор <saleroses@yandex.ru>
2023-01-20 16:21:13 +0300

    ver. 0.0001

--- js/script.js
+++ js/script.js
@@ -7,5 +7,20 @@ let guesses = 0;
 let isSunk = false;
 
 while (isSunk == false) {
-	guess = prompt ("Ready? Go! (enter a number 1-6):");
+	guess = prompt ("Ready? Go! (введи цифру 1-6):");
+	if (guess < 1 || guess > 6) {
+		alert("Введи правильную цифру!");
+	} else {
+		guesses = guesses+1;
+
+		if (guess== location1||guess== location2||guess== location3){
+			hits = hits+1;
+			if (hits == 3) {
+				isSunk = true;
+				alert ("Ты потопил(а) мой корабль!");
+			}
+		}
+	}
 }
+let stats = "Ты сделал(а) " + guesses + "попыток, " + "точность составила " + (3/guesses);
+alert(stats);
