b20fbfe32175cc8bd146c25a84f8063fa59a58b7
Егор <saleroses@yandex.ru>
2023-01-20 17:20:05 +0300

    добавлена точность в % и рандом

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-let location1 = 3;
-let location2 = 4;
-let location3 = 5;
+let randomLoc = Math.floor(Math.random() * 5);
+console.log(randomLoc)
+let location1 = randomLoc;
+let location2 = location1+1;
+let location3 = location2+1;
 let guess;
 let hits = 0;
 let guesses = 0;
 let isSunk = false;
 
 while (isSunk == false) {
-	guess = prompt ("Ready? Go! (введи цифру 1-6):");
-	if (guess < 1 || guess > 6) {
+	guess = prompt ("Ready? Go! (введи цифру 0-6):");
+	if (guess < 0 || guess > 6) {
 		alert("Введи правильную цифру!");
 	} else {
 		guesses = guesses+1;
@@ -24,5 +26,5 @@ while (isSunk == false) {
 		}
 	}
 }
-let stats = "Ты сделал(а) " + guesses + " попыток, " + "точность составила " + (3/guesses);
+let stats = "Ты сделал(а) " + guesses + " попыток, " + "точность составила " + (3/guesses*100) + "%";
 alert(stats);
